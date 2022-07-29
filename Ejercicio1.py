4832556cb6f60ca9173a83fdb6f0d376fe26a5bd
davopaco <107714070+davopaco@users.noreply.github.com>
2022-07-29 08:35:28 -0500

    Cambios ej 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -7,11 +7,24 @@ def es_primo(num1):
     else:
         return False
 
-print("===DETERMINAR NUMERO PRIMITIVOS===")
+def det_divisores(num1):
+    lista_divisores=[1]
+    for i in range(2,num1+1):
+        if(num1%i==0):
+            lista_divisores.append(i)
+    return lista_divisores
 
-num=int(input("Digite el numero entero primitivo a evaluar: "))
+
+print("===DETERMINAR NUMERO PRIMITIVOS===")
+try:
+    num=input("Digite el numero entero primitivo a evaluar: ")
+except:
+    "El numero debe ser entero."
 
 if(es_primo(num)):
     print("El numero es primitivo.")
 else:
     print("El numero es compuesto.")
+
+print("Los divisores son: ")
+print(det_divisores(num))
